bd07999ca1c4c1e18708e4bdf14eb01a7779968b
GAMING CHAUHAN <143026011+Gamingbuddyyy@users.noreply.github.com>
2023-09-27 19:46:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ if __name__ == "__main__":
     for i, CLIENT in enumerate(CLIENTS):
         try:
             CLIENT.start()
-            CLIENT.join_chat("TheAltron")
-            CLIENT.join_chat("AltronChats")
+            CLIENT.join_chat("Billagangh")
+            CLIENT.join_chat("Billagangh")
             print(f"---> Client {i+1} has been Started...")
         except Exception as e:
             print(e)
 
-    print("💞YOUR PY-ALTRON SPAM USERBOTS DEPLOYED SUCCESSFULLY 💞")
+    print("💞YOUR PY-SAHENSHAH SPAM USERBOTS DEPLOYED SUCCESSFULLY 💞")
     idle()
